7e0c9b2f44c5fc0100319d30cc4a51047ae350af
Edward T. Kaszubski <ekaszubski@gmail.com>
2016-10-18 11:39:23 -0700

    Add smoothing filter params to humanoid source/sink adapters.

--- src/humanoid_sink_adapter.cpp
+++ src/humanoid_sink_adapter.cpp
@@ -12,10 +12,63 @@ semio::ros::HumanoidSinkAdapter::HumanoidSinkAdapter( ::ros::NodeHandle const & 
 
 semio::HumanoidSink::Ptr semio::ros::HumanoidSinkAdapter::getHumanoidSink( std::string const & sink )
 {
-    std::string const & sink_type( sink == "param" ? _nh_rel.param( std::string( "humanoid_sink_type" ), _default_sink ) : sink );
+    std::string const & sink_type( sink == "param" ? _nh_rel.param( std::string( "humanoid_sink/type" ), _default_sink ) : sink );
 
-    if( sink_type == "ros" ) return std::make_shared<semio::ros::HumanoidSinkROS>( _nh_rel );
+    semio::HumanoidSink::Ptr result;
 
-    // none
-    return std::make_shared<semio::ros::HumanoidSinkROS>( ::ros::NodeHandle( _nh_rel, "/null" ) );
+    if( sink_type == "ros" ) result = std::make_shared<semio::ros::HumanoidSinkROS>( _nh_rel );
+    else result = std::make_shared<semio::ros::HumanoidSinkROS>( ::ros::NodeHandle( _nh_rel, "/null" ) );
+
+    // apply smoothing filter if specified via params
+    {
+        std::shared_ptr<semio::HumanoidSmoothingFilter> smoothing_filter_ptr;
+
+        typedef std::function<void(std::string const &)> _ParamFunc;
+        typedef std::pair<std::string, _ParamFunc> _ParamOp;
+
+        for( auto const & param_op : {
+            _ParamOp(
+                "humanoid_sink/filter/smoothing/add",
+                []( std::string const & param_name ){} ),
+            _ParamOp(
+                "humanoid_sink/filter/smoothing/position",
+                [this,&smoothing_filter_ptr]( std::string const & param_name ){
+                    smoothing_filter_ptr->setPositionSmoothing( semio::HumanoidSmoothingFilter::getSmoothing( 0.75, this->_nh_rel.param<double>( param_name, 0 ) ) );
+                } ),
+            _ParamOp(
+                "humanoid_sink/filter/smoothing/orientation",
+                [this,&smoothing_filter_ptr]( std::string const & param_name ){
+                    smoothing_filter_ptr->setOrientationSmoothing( semio::HumanoidSmoothingFilter::getSmoothing( 0.75, this->_nh_rel.param<double>( param_name, 0 ) ) );
+                } ),
+            _ParamOp(
+                "humanoid_sink/filter/smoothing/confidence",
+                [this,&smoothing_filter_ptr]( std::string const & param_name ){
+                    smoothing_filter_ptr->setConfidenceSmoothing( semio::HumanoidSmoothingFilter::getSmoothing( 0.75, this->_nh_rel.param<double>( param_name, 0 ) ) );
+                } ),
+            _ParamOp(
+                "humanoid_sink/filter/smoothing/window",
+                [this,&smoothing_filter_ptr]( std::string const & param_name ){
+                    smoothing_filter_ptr->setSmoothingWindow( this->_nh_rel.param<double>( param_name, 0 ) );
+                } ) } )
+        {
+            auto const & param_name( param_op.first );
+
+            if( _nh_rel.hasParam( param_name ) )
+            {
+                if( !smoothing_filter_ptr ) smoothing_filter_ptr = std::make_shared<semio::HumanoidSmoothingFilter>();
+
+                param_op.second( param_name );
+            }
+        }
+
+        // add a smoothing filter and a basic state filter (to remove untracked smoothed humanoids)
+        if( smoothing_filter_ptr )
+        {
+            auto & result_filter( result->getFilter() );
+            result_filter.addFilter( smoothing_filter_ptr );
+            result_filter.addFilter( std::make_shared<semio::HumanoidStateFilter>( semio::HumanoidStateFilter::getStandardFilterHumanoid() ) );
+        }
+    }
+
+    return result;
 }
--- src/humanoid_source_adapter.cpp
+++ src/humanoid_source_adapter.cpp
@@ -16,19 +16,72 @@ semio::ros::HumanoidSourceAdapter::HumanoidSourceAdapter( ::ros::NodeHandle cons
 
 semio::HumanoidSource::Ptr semio::ros::HumanoidSourceAdapter::getHumanoidSource( std::string const & source )
 {
-    std::string const & source_type( source == "param" ? _nh_rel.param( std::string( "humanoid_source_type" ), _default_source ) : source );
+    std::string const & source_type( source == "param" ? _nh_rel.param( std::string( "humanoid_source/type" ), _default_source ) : source );
 
-    if( source_type == "nite" ) return std::make_shared<semio::HumanoidSourceNiTE>();
-    else if( source_type == "openface" ) return std::make_shared<semio::HumanoidSourceOpenFace>();
+    semio::HumanoidSource::Ptr result;
+
+    if( source_type == "nite" ) result = std::make_shared<semio::HumanoidSourceNiTE>();
+    else if( source_type == "openface" ) result = std::make_shared<semio::HumanoidSourceOpenFace>();
     else if( source_type == "fullbody" )
     {
-        auto result( std::make_shared<semio::HumanoidSourceFullBody>() );
-        result->show_cropped_image_ = _nh_rel.param<bool>( std::string( "show_cropped_image" ), false );
-        return result;
+        auto local_result( std::make_shared<semio::HumanoidSourceFullBody>() );
+        local_result->show_cropped_image_ = _nh_rel.param<bool>( std::string( "humanoid_source/fullbody/show_cropped_image" ), false );
+        result = local_result;
     }
-    else if( source_type == "ros" ) return std::make_shared<semio::ros::HumanoidSourceROS>( _nh_rel );
+    else if( source_type == "ros" ) result = std::make_shared<semio::ros::HumanoidSourceROS>( _nh_rel );
     else if( source_type == "none" ) return std::make_shared<semio::ros::HumanoidSourceROS>( ::ros::NodeHandle( _nh_rel, "/null" ) );
+    else result = std::make_shared<HumanoidSourceVirtual>();
 
-    // virtual
-    return std::make_shared<HumanoidSourceVirtual>();
+    // apply smoothing filter if specified via params
+    {
+        std::shared_ptr<semio::HumanoidSmoothingFilter> smoothing_filter_ptr;
+
+        typedef std::function<void(std::string const &)> _ParamFunc;
+        typedef std::pair<std::string, _ParamFunc> _ParamOp;
+
+        for( auto const & param_op : {
+            _ParamOp(
+                "humanoid_source/filter/smoothing/add",
+                []( std::string const & param_name ){} ),
+            _ParamOp(
+                "humanoid_source/filter/smoothing/position",
+                [this,&smoothing_filter_ptr]( std::string const & param_name ){
+                    smoothing_filter_ptr->setPositionSmoothing( semio::HumanoidSmoothingFilter::getSmoothing( 0.75, this->_nh_rel.param<double>( param_name, 0 ) ) );
+                } ),
+            _ParamOp(
+                "humanoid_source/filter/smoothing/orientation",
+                [this,&smoothing_filter_ptr]( std::string const & param_name ){
+                    smoothing_filter_ptr->setOrientationSmoothing( semio::HumanoidSmoothingFilter::getSmoothing( 0.75, this->_nh_rel.param<double>( param_name, 0 ) ) );
+                } ),
+            _ParamOp(
+                "humanoid_source/filter/smoothing/confidence",
+                [this,&smoothing_filter_ptr]( std::string const & param_name ){
+                    smoothing_filter_ptr->setConfidenceSmoothing( semio::HumanoidSmoothingFilter::getSmoothing( 0.75, this->_nh_rel.param<double>( param_name, 0 ) ) );
+                } ),
+            _ParamOp(
+                "humanoid_source/filter/smoothing/window",
+                [this,&smoothing_filter_ptr]( std::string const & param_name ){
+                    smoothing_filter_ptr->setSmoothingWindow( this->_nh_rel.param<double>( param_name, 0 ) );
+                } ) } )
+        {
+            auto const & param_name( param_op.first );
+
+            if( _nh_rel.hasParam( param_name ) )
+            {
+                if( !smoothing_filter_ptr ) smoothing_filter_ptr = std::make_shared<semio::HumanoidSmoothingFilter>();
+
+                param_op.second( param_name );
+            }
+        }
+
+        // add a smoothing filter and a basic state filter (to remove untracked smoothed humanoids)
+        if( smoothing_filter_ptr )
+        {
+            auto & result_filter( result->getFilter() );
+            result_filter.addFilter( smoothing_filter_ptr );
+            result_filter.addFilter( std::make_shared<semio::HumanoidStateFilter>( semio::HumanoidStateFilter::getStandardFilterHumanoid() ) );
+        }
+    }
+
+    return result;
 }
